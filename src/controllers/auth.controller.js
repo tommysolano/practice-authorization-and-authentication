46781837a7aaf3002a198063155b46295632f644
tommy <tommysolano18@hotmail.com>
2021-08-30 18:09:36 -0500

    adding jwt and roles in signup

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,18 +1,35 @@
 import User from "../models/User"
+import jwt from "jsonwebtoken"
+import config from "../config"
 
 export const signUp = async (req, res) => {
 
     const {username, email, password, roles} = req.body
 
+    //revisa los datos ingresados y encripta el password
     const newUser = new User({
         username,
         email,
         password: await User.encryptPassword(password)
     })
 
+    //verifica si se recive un role y lo asigna caso contrario se le asigna el role user
+    if (roles) {
+        const foundRoles = await Role.find({name: {$in: roles}})
+        newUser.roles = foundRoles.map(role => role._id)  
+    } else {
+        const role = await Role.findOne({ name: "user"})
+        newUser.roles = [role._id]
+    }
+
+    //guarda en la base de datos
     const savedUser = await newUser.save()
 
-    res.json("signUp")
+    const token = jwt.sign({id: savedUser._id}, config.SECRET, {
+        expiresIn: 86400
+    })
+
+    res.json({token})
 }
 
 
